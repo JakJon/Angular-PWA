ee2ecaefe63bfe2a7edf4279278096cdb814f245
Jacob 🐳Jones <jonej134@ferris.edu>
2018-08-13 01:17:14 -0400

    Connected to chuck norris api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
+import { DataService } from './data.service';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Jake';
+
+  update: boolean = false;
+  joke: any;
+
+  constructor(updates: SwUpdate, private data: DataService) {
+    updates.available.subscribe(event => {
+
+      updates .activateUpdate().then(() => document.location.reload());
+    })
+  }
+
+  ngOnInit() {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.data.returnTheJokes().subscribe(res => {
+      this.joke = res;
+    })
+  }
 }
